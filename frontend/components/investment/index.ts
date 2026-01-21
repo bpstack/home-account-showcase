b0d20d38c3dd21e45d00b797cbf30bc94fbbc237
razyd <contact.bstack@gmail.com>
2026-01-21 21:34:54 +0100

    feat(investment): add component exports barrel file
    Bundle all investment components for clean imports:
    - Re-export all components from single entry point
    - Makes importing multiple components cleaner

---
+++ frontend/components/investment/index.ts
@@ -0,0 +1,12 @@
+// components/investment/index.ts
+// Export all investment components
+
+export { InvestmentOverview, RiskBadge } from './InvestmentOverview'
+export { MarketPricesWidget } from './MarketPricesWidget'
+export { MarketSummary } from './MarketSummary'
+export { DisclaimerAlert, ChatDisclaimer } from './DisclaimerAlert'
+export { AIChat, AIChatCompact } from './AIChat'
+export { ProfileForm } from './ProfileForm'
+export { Recommendations } from './Recommendations'
+export { Simulator } from './Simulator'
+export { InvestmentWidget } from './InvestmentWidget'
